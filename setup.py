5e7d062900fc1388df4b28a504ca2216dbe70d70
martinlanton <lantonmartin@gmail.com>
2020-07-25 16:23:01 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+name = 'template_project'
+
+
+setup(name=name,
+      version='0.1',
+      packages=find_packages(),
+      install_requires=['pytest'])
---
+++ tests/test_one.py
@@ -0,0 +1,5 @@
+from one.module_one import my_first_var as v
+
+
+def test_module_one():
+    assert v
