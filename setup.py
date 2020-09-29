4cb1e6b491d048ef97f98095bc7088a99f238a09
martinlanton <lantonmartin@gmail.com>
2020-09-28 17:01:42 -0700

    -Added dependency

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
 
 name = 'template_project'
+author = "Martin L'Anton"
+author_email = "lantonmartin@gmail.com"
+url = "https://github.com/martinlanton/ComponentAssembler"
 
-
-setup(name=name,
-      version='0.1',
-      package_dir={'': 'src'},
-      packages=find_packages(where='src'),
-      install_requires=['pytest'])
+setup(
+    name=name,
+    version='0.1.0',
+    author=author,
+    author_email=author_email,
+    url=url,
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'pytest',
+        'pytest-cov',
+    ]
+)
